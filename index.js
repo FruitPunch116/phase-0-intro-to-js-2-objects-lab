aa64efda39d02a690ccf02b3a6599111efcf276c
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-07-12 21:18:06 -0400

    modify functions and log the process

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-// Write your solution in this file!
 const employee = {
     name: "Sam",
     streetAddress: "11 Broadway",
-}
+};
+// console.log(employee);
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
     const newEmployee = {...employee};
     newEmployee[key] = value;
+    // console.log(newEmployee);
+    // console.log(key);
+    // console.log(employee);
+    // console.log(value)
     return newEmployee;
-}
+};
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-  const newEmployee = {
-      ...employee, 
-      [key] : value
-      };
-
-      employee[key]= value;
-
-  return newEmployee;
-}
+  employee[key]= value;
+  // console.log(employee);
+  //console.log(key);
+  return employee;
+};
 
 function deleteFromEmployeeByKey(employee, key) {
   const newEmployee = {...employee};
-  
   delete newEmployee[key];
-
+  // console.log(employee);
+  // console.log(key);
+  // console.log(newEmployee);
   return newEmployee;
-}
+};
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
-  const newEmployee = {
-    employee,
-    [key] : "Sam"
-   };
-
+  employee[key];
   delete employee[key];
+  // console.log(employee)
+  // console.log(employee[key]);
 
-  console.log(newEmployee);
-  console.log(employee[key]);
   return employee;
-}
+};
